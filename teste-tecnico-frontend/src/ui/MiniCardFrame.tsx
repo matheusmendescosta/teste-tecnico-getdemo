c1d254427236a6acea49589e981852ed4ed8941d
matheusmendescosta <matheus_mendess@live.com>
2024-08-26 13:53:19 -0300

    sorted frames by order

--- teste-tecnico-frontend/src/ui/MiniCardFrame.tsx
+++ teste-tecnico-frontend/src/ui/MiniCardFrame.tsx
@@ -8,12 +8,14 @@ type MiniCardFrameProps = {
 };
 
 export const MiniCardFrame = ({ frames, onFrameSelect }: MiniCardFrameProps) => {
+  const sortedFrames = [...frames].sort((a, b) => a.order - b.order);
+
   return (
     <div className="fixed top-0 bottom-0 right-0 h-screen w-64 overflow-y-auto bg-white border-l border-gray-200">
       <div className="p-4">
         <h3 className="text-lg font-semibold mb-2 text-center">Lista de Frames</h3>
         <ul>
-          {frames.map((frame, index) => {
+          {sortedFrames.map((frame, index) => {
             return (
               <li key={index} className="mb-2">
                 <p className="text-base text-center font-bold">Frame {index + 1}</p>
