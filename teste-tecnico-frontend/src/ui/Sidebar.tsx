5842fdb6621ad74babe817d1b8e2e4bd74057f61
matheusmendescosta <matheus_mendess@live.com>
2024-08-26 13:56:17 -0300

    refactor components

--- teste-tecnico-frontend/src/ui/Sidebar.tsx
+++ teste-tecnico-frontend/src/ui/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiChartPie, HiUser, HiViewBoards } from "react-icons/hi";
 
 type SidebarMenu = {
   children: React.ReactNode;
